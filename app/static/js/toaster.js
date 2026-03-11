d38afc9c263d87ccf2239adb4ebd1538718f91a7
ecrou-exact <geffetheo@gmail.com>
2026-03-11 14:10:52 +0100

    add: [components] new pagination and toast components

--- app/static/js/toaster.js
+++ app/static/js/toaster.js
@@ -2,36 +2,76 @@ const { nextTick, ref } = Vue
 
 export const message_list = ref([])
 
-export async function display_toast(res) {
-	let loc = await res.json()
-	let id = Math.random()
-	let message_loc = {"message": loc["message"], "toast_class": loc["toast_class"], "id": id}
-	message_list.value.push(message_loc)
-	await nextTick()
-	const toastLiveExample = document.getElementById('liveToast-'+id)
-	const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample)
-	toastBootstrap.show()
-	toastLiveExample.addEventListener('hidden.bs.toast', () => {
-		let index = message_list.value.indexOf(message_loc)
-		if(index > -1)
-			message_list.value.splice(index, 1)
-	})
+function manage_icon(toast_class){
+	let icon = ""
+	if(toast_class){
+		switch(toast_class){
+			case "success-subtle":
+				icon = "fas fa-check"
+				break
+			case "warning-subtle":
+				icon = "fas fa-triangle-exclamation"
+				break
+			case "danger-subtle":
+				icon = "fas fa-xmark"
+				break
+		}
+	}
+	return icon
 }
 
-export async function prepare_toast(res){
-	let loc = await res.json()
-	return {"message": loc["message"], "toast_class": loc["toast_class"], "id": Math.random()}
+/**
+ * Creates and displays a toast message.
+ * @param {string} message - The message to be displayed in the toast.
+ * @param {string} toast_class - The CSS class to style the toast (e.g., "success-subtle", "warning-subtle", "danger-subtle").
+ * @param {boolean} not_hide - If true, the toast will not auto-hide and will require manual dismissal.
+ * @param {string} icon - The CSS class for the icon to be displayed in the toast (e.g., "fas fa-check"). If not provided, it will be determined based on the toast_class.
+ */
+export async function create_message(message, toast_class, not_hide, icon) {
+    let id = Math.random();
+    
+    if (!icon) {
+        icon = manage_icon(toast_class);
+    }
+    
+    let message_loc = { "message": message, "toast_class": toast_class, "id": id, "icon": icon };
+    
+    message_list.value.push(message_loc);
+    await nextTick();
+    
+    const toastLiveExample = document.getElementById('liveToast-' + id);
+
+    if (not_hide) {
+        toastLiveExample.setAttribute("data-bs-autohide", "false");
+    }
+
+    const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample);
+    toastBootstrap.show();
+
+    setTimeout(() => {
+            let index = message_list.value.indexOf(message_loc);
+            if (index > -1) {
+                message_list.value.splice(index, 1);
+            }
+        }, 5500);
+
+    toastLiveExample.addEventListener('hidden.bs.toast', () => {
+        let index = message_list.value.indexOf(message_loc);
+        if (index > -1) {
+            message_list.value.splice(index, 1);
+        }
+    });
 }
 
-export async function display_prepared_toast(message){
-	message_list.value.push(message)
-	await nextTick()
-	const toastLiveExample = document.getElementById('liveToast-'+message.id)
-	const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample)
-	toastBootstrap.show()
-	toastLiveExample.addEventListener('hidden.bs.toast', () => {
-		let index = message_list.value.indexOf(message)
-		if(index > -1)
-			message_list.value.splice(index, 1)
-	})
+export async function display_toast(res, not_hide=false) {
+	let loc = await res.json()
+	
+	if (typeof loc["message"] == "object"){
+		for(let index in loc["message"]){
+			await create_message(loc["message"][index], loc["toast_class"][index], not_hide, loc["icon"])
+		}
+	}
+	else{
+		await create_message(loc["message"], loc["toast_class"], not_hide, loc["icon"])
+	}
 }
